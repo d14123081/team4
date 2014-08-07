40f3f5e7493c725bdb15442d8a091feb3c0847f4
Eoin Kernan <eoin.kernan@mydit.ie>
2014-08-07 14:46:39 +0100

    Removed Test class. Added Controller class, created packages for model and view.

---
+++ src/team4/retailsystem/Controller.java
@@ -0,0 +1,15 @@
+package team4.retailsystem;
+
+/**
+ * A controller class to manipulate the model and react to events from the view
+ * 
+ * @author d14123081
+ *
+ */
+public class Controller {
+
+	public Controller() {
+		
+	}
+
+}
