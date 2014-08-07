7490fa7cbabbe40bbea33bbe413c8cf66e419917
Eoin Kernan <eoin.kernan@mydit.ie>
2014-08-07 14:49:47 +0100

    Added test model and view objects (ModelObject, ViewObject)

---
+++ src/team4/retailsystem/model/ModelObject.java
@@ -0,0 +1,15 @@
+package team4.retailsystem.model;
+
+/**
+ * Test class for representing a model object
+ * 
+ * @author d14123081
+ *
+ */
+public class ModelObject {
+
+	public ModelObject() {
+		
+	}
+
+}
---
+++ src/team4/retailsystem/view/ViewObject.java
@@ -0,0 +1,15 @@
+package team4.retailsystem.view;
+
+/**
+ * Test class for representing a view object
+ * 
+ * @author d14123081
+ *
+ */
+public class ViewObject {
+
+	public ViewObject() {
+		
+	}
+
+}
