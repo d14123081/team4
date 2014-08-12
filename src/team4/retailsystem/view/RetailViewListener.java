928d63035b823b1fc08178010b878d24afa9d498
AlanKavo92 <alankavo92@hotmail.com>
2014-08-12 14:34:11 +0100

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	src/team4/retailsystem/model/LineItem.java
    	src/team4/retailsystem/model/Product.java

---
+++ src/team4/retailsystem/view/RetailViewListener.java
@@ -0,0 +1,12 @@
+package team4.retailsystem.view;
+
+/**
+ * An interface to describe the behaviour of any object that is listening to the
+ * retail view for events.
+ * 
+ * @author Eoin Kernan D14123081
+ *
+ */
+public interface RetailViewListener {
+	//not implemented yet
+}
