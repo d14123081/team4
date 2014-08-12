5b7145c9ed9577617bb94377d22240937389816c
Eoin Kernan <eoinkernan@gmail.com>
2014-08-12 15:01:40 +0100

    Removed calculator MVC pattern example classes, no longer required. Restored retail model/view/controller/listener classes that disappeared in a previous commit.

---
+++ src/team4/retailsystem/RetailSystem.java
@@ -0,0 +1,29 @@
+package team4.retailsystem;
+
+import team4.retailsystem.controller.RetailSystemController;
+import team4.retailsystem.model.RetailSystemModel;
+import team4.retailsystem.view.RetailSystemView;
+
+/**
+ * The main retail system driver. Creates model, view, and controller classes.
+ * 
+ * @author Eoin Kernan D14123081
+ *
+ */
+public class RetailSystem {
+
+	private RetailSystemController controller;
+	private RetailSystemModel model;
+	private RetailSystemView view;
+	
+	public RetailSystem() {
+		view = new RetailSystemView();
+		model = new RetailSystemModel();
+		controller = new RetailSystemController(model, view);
+	}
+
+	public static void main(String[] args) {
+		new RetailSystem();
+	}
+
+}
---
+++ src/team4/retailsystem/model/RetailModelListener.java
@@ -0,0 +1,12 @@
+package team4.retailsystem.model;
+
+/**
+ * An interface to describe the behaviour of any object that is listening to the
+ * retail model for changes.
+ * 
+ * @author Eoin Kernan D14123081
+ *
+ */
+public interface RetailModelListener {
+	
+}
