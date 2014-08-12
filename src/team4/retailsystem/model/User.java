8d85f2539dcd4268bae5777dcb23cf51bcc805d9
Szymon Zielinski <szymon.zielinski14@gmail.com>
2014-08-12 15:21:24 +0100

    First implementation for User.

--- src/team4/retailsystem/model/User.java
+++ src/team4/retailsystem/model/User.java
@@ -1,12 +1,38 @@
 package team4.retailsystem.model;
 
 public class User {
-	
-	public int getAuthorizationLevel(){
-		return -1;
+	private static int idCounter = 0;
+	private int id;
+	private int authorizationLevel;
+
+	/*
+	 * Assumption - Users will be accessed through the Database method
+	 * authorizeUser(String, String). Hence, for security protected access to
+	 * constructors.
+	 */
+	protected User(int authorizationLevel) {
+		setAuthorizationLevel(authorizationLevel);
+		setId(idCounter++);
 	}
-	
-	public int getID(){
-		return -1;
+
+	protected User(int id, int authorizationLevel) {
+		setAuthorizationLevel(authorizationLevel);
+		setId(id);
+	}
+
+	public int getAuthorizationLevel() {
+		return authorizationLevel;
+	}
+
+	public int getID() {
+		return id;
+	}
+
+	private void setId(int id) {
+		this.id = id;
+	}
+
+	private void setAuthorizationLevel(int authorizationLevel) {
+		this.authorizationLevel = authorizationLevel;
 	}
 }
