c0f2f7958f1c35c148d3aef9c2db0eaaa04c9a55
Szymon Zielinski <szymon.zielinski14@gmail.com>
2014-09-01 17:17:07 +0100

    db: add methods to generate random data
    - able to generate any number of any data items
    - read from .txt files
    - implemented into SampleDataGenerator

--- test/team4/retailsystem/integration/SampleDataGenerator.java
+++ test/team4/retailsystem/integration/SampleDataGenerator.java
@@ -1,11 +1,11 @@
 package team4.retailsystem.integration;
 
-import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Random;
+import java.util.Scanner;
 
 import team4.retailsystem.model.Customer;
 import team4.retailsystem.model.Database;
@@ -25,6 +25,10 @@ import team4.retailsystem.model.User;
  */
 public class SampleDataGenerator {
 	private static Random random = new Random(System.currentTimeMillis());
+	
+	private static ArrayList<String> companyNames = new ArrayList<String>();
+	private static ArrayList<String> addresses = new ArrayList<String>();
+	private static ArrayList<String> productNames = new ArrayList<String>();
 
 	private static void generateDatabase() {
 		generateUsers();
@@ -42,27 +46,21 @@ public class SampleDataGenerator {
 	}
 
 	private static String getCompanyName() {
-		FileReader fr = null;
-		try {
-			fr = new FileReader("names.txt");
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-			return null;
+		if(companyNames.size() == 0){
+			File txt = new File("names.txt");
+			try {
+				Scanner scan = new Scanner(txt);				
+				while(scan.hasNextLine()){
+					companyNames.add(scan.nextLine());
+				}				
+				scan.close();		
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+				return null;
+			}	
 		}
-		BufferedReader br = new BufferedReader(fr);
-		try {
-			String line = br.readLine();
-			int count = random.nextInt(4500);
-			for (int j = 0; j < count; j++) {
-				line = br.readLine();
-			}
-			br.close();
-			fr.close();
-			return line;
-		} catch (IOException e) {
-			e.printStackTrace();
-			return null;
-		}
+		
+		return companyNames.remove(random.nextInt(companyNames.size()));
 	}
 
 	private static String getEmail(String name) {
@@ -71,59 +69,50 @@ public class SampleDataGenerator {
 
 	private static String getTelephone() {
 		String tel = "";
-		for(int i=0; i<random.nextInt(3)+9; i++){
+		for(int i=0; i<7; i++){
 			tel += random.nextInt(9);
 		}
 		return tel;
 	}
 
 	private static String getAddress() {
-		FileReader fr = null;
-		try {
-			fr = new FileReader("locations.txt");
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-			return null;
+		if(addresses.size() == 0){
+			File txt = new File("locations.txt");
+			try {
+				Scanner scan = new Scanner(txt);				
+				while(scan.hasNextLine()){
+					addresses.add(scan.nextLine());
+				}				
+				scan.close();		
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+				return null;
+			}	
 		}
-		BufferedReader br = new BufferedReader(fr);
-		try {
-			String line = br.readLine();
-			for (int j = 0; j < random.nextInt(60); j++) {
-				line = br.readLine();
-			}
-			br.close();
-			fr.close();
-			return line;
-		} catch (IOException e) {
-			e.printStackTrace();
-			return null;
-		}
+		
+		return addresses.get(random.nextInt(addresses.size()));
 	}
 
 	private static String getProductName() {
-		FileReader fr = null;
-		try {
-			fr = new FileReader("products.txt");
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-			return null;
+		if(productNames.size() == 0){
+			File txt = new File("products.txt");
+			try {
+				Scanner scan = new Scanner(txt);				
+				while(scan.hasNextLine()){
+					productNames.add(scan.nextLine());
+				}				
+				scan.close();		
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+				return null;
+			}	
 		}
-		BufferedReader br = new BufferedReader(fr);
-		try {
-			String line = br.readLine();
-			for (int j = 0; j < random.nextInt(180); j++) {
-				line = br.readLine();
-			}
-			br.close();
-			fr.close();
-			return line;
-		} catch (IOException e) {
-			e.printStackTrace();
-			return null;
-		}
+		
+		return productNames.remove(random.nextInt(productNames.size()));
 	}
 
 	private static void generateSuppliers(int amount) {
+		System.out.print("\nGenerating suppliers");
 		for(int i=0; i<amount; i++){
 			String name = getCompanyName();
 			String address = getAddress();
@@ -131,10 +120,12 @@ public class SampleDataGenerator {
 			String telephone = getTelephone();
 			
 			Database.getInstance().addSupplier(new Supplier(name, address, email, telephone));
+			System.out.print(".");
 		}
 	}
 
 	private static void generateCustomers(int amount) {
+		System.out.print("\nGenerating customers");
 		for(int i=0; i<amount; i++){
 			String name = getCompanyName();
 			String address = getAddress();
@@ -142,22 +133,27 @@ public class SampleDataGenerator {
 			String telephone = getTelephone();
 			
 			Database.getInstance().addCustomer(new Customer(name, telephone, email, address));
+			System.out.print(".");
 		}
 	}
 	
 	private static void generateProducts(int amount){
+		System.out.print("\nGenerating products");
 		ArrayList<Supplier> suppliers = Database.getInstance().getSuppliers();
 		for(int i=0; i<amount; i++){
 			String name = getProductName();
-			double cost = random.nextDouble()*1000;
-			double markup = random.nextDouble()*400;
-			int stockLevel = random.nextInt(80);
+			double cost = random.nextDouble()*15;
+			double markup = random.nextDouble()*250;
+			int stockLevel = random.nextInt(200);
 			Supplier s = suppliers.get(random.nextInt(suppliers.size()-1));
+			
 			Database.getInstance().addProduct(new Product(name, cost, markup, stockLevel, s));
+			System.out.print(".");
 		}
 	}
 	
 	private static void generateOrders(int amount){
+		System.out.print("\nGenerating orders");
 		ArrayList<Supplier> suppliers = Database.getInstance().getSuppliers();
 		ArrayList<Product> products = Database.getInstance().getProducts();
 		for(int i=0; i<amount; i++){
@@ -167,11 +163,16 @@ public class SampleDataGenerator {
 				lineItems.add(new LineItem(products.get(random.nextInt(products.size()-1)).getID(), random.nextInt(25)+1));
 			}
 			Supplier s = suppliers.get(random.nextInt(suppliers.size()-1));
-			Database.getInstance().addOrder(new Order(1, s, 0, lineItems));
+			Date date = new Date();
+			date = new Date(date.getTime() - 1000*60*60*24*(random.nextInt(31*6)));
+			
+			Database.getInstance().addOrder(new Order(1, s, 0, lineItems, 0, date));
+			System.out.print(".");
 		}
 	}
 	
 	private static void generateInvoices(int amount){
+		System.out.print("\nGenerating invoices");
 		ArrayList<Product> products = Database.getInstance().getProducts();
 		ArrayList<Customer> customers = Database.getInstance().getCustomers();
 		for(int i=0; i<amount; i++){
@@ -181,12 +182,27 @@ public class SampleDataGenerator {
 				lineItems.add(new LineItem(products.get(random.nextInt(products.size()-1)).getID(), random.nextInt(25)+1));
 			}
 			Customer c = customers.get(random.nextInt(customers.size()-1));
-			Database.getInstance().addInvoice(new Invoice(lineItems, c));
+			Date date = new Date();
+			date = new Date(date.getTime() - 1000*60*60*24*(random.nextInt(31*6)));
+			
+			Database.getInstance().addInvoice(new Invoice(lineItems, c, date));
+			System.out.print(".");
 		}
 	}
 	
 	private static void generateDeliveries(int amount){
-		//TODO: complete the method body
+		System.out.print("\nGenerating deliveries");
+		ArrayList<Order> orders = Database.getInstance().getOrders();
+		
+		for(int i=0; i<amount; i++){
+			int randOrderID = random.nextInt(orders.size()-1);
+			Order o = orders.get(randOrderID);
+			Date date = new Date(o.getOrderDate().getTime() + 1000*60*60*24*(random.nextInt(3)+2));
+			
+			Database.getInstance().addDelivery(new Delivery(o.getSupplier(), o.getID(), date, 0));
+			orders.remove(randOrderID);
+			System.out.print(".");
+		}
 	}
 
 	private static void generateUsers() {
@@ -268,10 +284,11 @@ public class SampleDataGenerator {
 		generateProducts(150);
 		generateOrders(150);
 		generateInvoices(150);		
+		generateDeliveries(80);
 	}
 
 	public static void main(String[] args) {
-		generateDatabase();
-		//generateRandomDatabase();
+		//generateDatabase();
+		generateRandomDatabase();
 	}
 }
